cdbc01475ca9239bc9e4afcef704c24eecea1557
Shrey Patel <shreypatel11@hotmail.com>
2022-06-27 14:12:36 -0400

    Create database and tables for students and cohorts

---
+++ migrations/students_cohorts.sql
@@ -0,0 +1,24 @@
+-- CREATE TABLE cohorts (
+--   id SERIAL PRIMARY KEY,
+--   name VARCHAR(50),
+--   start_date VARCHAR(DATE),
+--   end_date VARCHAR(DATE)
+-- );
+
+CREATE TABLE cohorts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  start_date DATE,
+  end_date DATE
+);
+
+CREATE TABLE students (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255),
+  phone VARCHAR(32),
+  github VARCHAR(255),
+  start_date DATE,
+  end_date DATE,
+  cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
+);
